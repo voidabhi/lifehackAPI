902503e01726706981465bcabc54cacae44932d6
voidabhi <void.aby@gmail.com>
2014-04-19 23:25:24 +0530

    init

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+Python API for LifeHack.
+
+@author Abhijeet Mohan
+"""
+from .version import VERSION
+
+__title__ = 'lifehack'
+__version__ = VERSION
+__author__ = 'Abhijeet Mohan'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2014 Abhijeet Mohan'
---
+++ lh.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python
+
+"""
+Life Hack API
+Unofficial Python API for Life Hack.
+
+@author Abhijeet Mohan
+@email void.aby@gmail.com
+"""
+
+
+import requests
+from bs4 import BeautifulSoup
+
+from .constants import BASE_URL
